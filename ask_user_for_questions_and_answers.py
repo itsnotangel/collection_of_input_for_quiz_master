786107c021826b28732a7ab3189a86ec448eff80
itsnotangel <angellaikaclarito04@gmail.com>
2025-04-09 15:57:38 +0800

    Added the main title of the window saying 'WELCOME TO THE QUIZZLER using an Arial font.

--- ask_user_for_questions_and_answers.py
+++ ask_user_for_questions_and_answers.py
@@ -8,6 +8,10 @@ class TheQuizzler:
         self.root = root
         self.root.geometry("400x400")
 
+        # Adding the main title of the window
+        main_title_label = tk.Label(root, text="WELCOME TO THE QUIZZLER", font=("Arial", 18))
+        main_title_label.pack(fill=tk.X, pady=(10, 0))
+        
         # Getting the window size and printing it to the console (Only for checking the size)
         window_width = self.root.winfo_width()
         window_height = self.root.winfo_height()
