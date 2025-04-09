f63960782c652633f969869814bd30a0d9c9d582
itsnotangel <angellaikaclarito04@gmail.com>
2025-04-09 20:53:29 +0800

    Added an instruction label to help user how to properly fill out each field. Also adjusted the window size to 500x410.

--- ask_user_for_questions_and_answers.py
+++ ask_user_for_questions_and_answers.py
@@ -7,11 +7,15 @@ class TheQuizzler:
     def __init__(self, root):
         self.root = root
         self.root.title("The Quizzler - The ultimate Quiz in the Making")
-        self.root.geometry("500x400")
+        self.root.geometry("500x410")
 
         # Adding the main title of the window
         main_title_label = tk.Label(root, text="WELCOME TO THE QUIZZLER", font=("Arial", 18, "bold"), fg="darkred")
         main_title_label.pack(fill=tk.X, pady=(10, 0))
+
+        # Adding an instruction label to guide the user on how to use the application
+        instruction = tk.Label(root, text="Enter question, answer (strictly letter only from A-D), and four choices.", font=("Arial", 10), fg="grey")
+        instruction.pack(fill=tk.X, pady=(0, 10))
 
         # Adding the question box for user to input their questions
         tk.Label(text="Question:", font=("Arial", 8, "bold"), fg="darkred", anchor="w").pack(fill=tk.X, padx=20, pady=(10, 0))
