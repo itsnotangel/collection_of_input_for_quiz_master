963b6cb35a8cea8801f03914850e5c853ff0ff43
itsnotangel <angellaikaclarito04@gmail.com>
2025-04-09 14:00:39 +0800

    Added TheQuizzler class definition, printed the window size for checking purposes, and also added the entry point for the program to run the tkinter app.

--- ask_user_for_questions_and_answers.py
+++ ask_user_for_questions_and_answers.py
@@ -1,3 +1,20 @@
 # Imported the libraries
 import tkinter as tk
 from tkinter import messagebox
+
+# The Quizzler class definition, which sets up the main app window and specific dimension
+class TheQuizzler:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("50x50")
+
+        # Getting the window size and printing it to the console (Only for checking the size)
+        window_width = self.root.winfo_width()
+        window_height = self.root.winfo_height()
+        print(f"Window Size: {window_width}x{window_height}")
+
+# Initializes the main Tkinter window and runs the event loop to start the GUI application
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TheQuizzler(root)
+    root.mainloop()
