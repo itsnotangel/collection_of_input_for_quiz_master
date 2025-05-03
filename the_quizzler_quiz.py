36b58071d5b6ac7bc48a9610ebeca6e8d32385cf
itsnotangel <angellaikaclarito04@gmail.com>
2025-05-03 23:11:48 +0800

    Created main frame with padding and border, then added the title and subtitle labels inside it.

--- the_quizzler_quiz.py
+++ the_quizzler_quiz.py
@@ -10,12 +10,16 @@ class TheQuizzler:
         self.root = root
         self.root.geometry("600x600")
 
+        # Creating the main frame 
+        main_frame = tk.Frame(root, bd=5, relief="groove", padx=20, pady="20")
+        main_frame.pack(fill="both", padx=20, pady=20)
+
         # Adding the main title 
-        main_title_label = tk.Label(text="THE QUIZZLER", font=("Arial", 30, "bold"), fg="darkred")
+        main_title_label = tk.Label(main_frame, text="THE QUIZZLER", font=("Arial", 30, "bold"), fg="darkred")
         main_title_label.pack(fill=tk.X, pady=(10, 0))
 
         # Adding the subtitle
-        sub_title_label = tk.Label(text="THE ULTIMATE QUIZ EDITION", font=("Arial", 20, "bold"), fg="darkred")
+        sub_title_label = tk.Label(main_frame, text="THE ULTIMATE QUIZ EDITION", font=("Arial", 20, "bold"), fg="darkred")
         sub_title_label.pack(fill=tk.X, pady=(0, 10))
 
 # Initializes the main Tkinter window and runs the event loop to start the GUI application
